15fe3db496bf7b35b7e7e3ce0251069a1720587a
Alyssa <alawton1@my.hpu.edu>
2022-11-16 16:55:24 -1000

    Initial setup

---
+++ src/main/java/dis_mutex/Server.java
@@ -0,0 +1,12 @@
+package dis_mutex;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Server {
+    private static final Logger logger = LoggerFactory.getLogger(Server.class);
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
